6e40bd1ec3c00505496f0fb44ada8855fdbd3e23
Ramon Ziai <ramon.ziai@uni-tuebingen.de>
2020-05-30 17:35:14 +0200

    make isaac owner of db

--- docker-entrypoint-initdb.d/01-setup-isaac-db.sh
+++ docker-entrypoint-initdb.d/01-setup-isaac-db.sh
@@ -4,6 +4,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER isaac;
     ALTER USER isaac PASSWORD 'b4ck3nd!';
-    CREATE DATABASE isaac;
-    GRANT ALL PRIVILEGES ON DATABASE isaac TO isaac;
+    CREATE DATABASE isaac OWNER isaac;
 EOSQL
